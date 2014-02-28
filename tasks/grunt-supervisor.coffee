28fdba1d76a323a6775c427fedbf563d54fa877b
Leny <info@flatland.be>
2014-02-28 18:32:34 +0100

    Task code

--- tasks/grunt-supervisor.coffee
+++ tasks/grunt-supervisor.coffee
@@ -13,14 +13,27 @@ supervisor = require "supervisor"
 module.exports = ( grunt ) ->
 
     grunt.registerMultiTask "supervisor", "Runs a supervisor monitor of your node.js server.", ->
-
         @async()
 
         aOptions = []
+        oOptions = @data.options
 
+        aOptions.push "--watch", oOptions.watch.join "," if grunt.util.kindOf( oOptions.watch ) is "array"
+        aOptions.push "--ignore", oOptions.ignore.join "," if grunt.util.kindOf( oOptions.ignore ) is "array"
+        aOptions.push "--extensions", oOptions.extensions.join "," if grunt.util.kindOf( oOptions.extensions ) is "array"
 
+        aOptions.push "--exec", oOptions.exec if grunt.util.kindOf( oOptions.exec ) is "string"
 
-        aOptions.push "--"
-        aOptions.push @data.script
+        aOptions.push "--poll-interval", "#{ oOptions.pollInterval }" if grunt.util.kindOf( oOptions.pollInterval ) is "number"
+        aOptions.push "--no-restart-on", oOptions.noRestartOn if grunt.util.kindOf( oOptions.noRestartOn ) is "string" and ( oOptions.noRestartOn is "error" and oOptions.noRestartOn is "exit" )
+
+        aOptions.push "--debug" if oOptions.debug is yes
+        aOptions.push "--debug-brk" if oOptions.debugBrk is yes
+        aOptions.push "--harmony" if oOptions.harmony is yes
+        aOptions.push "--force-watch" if oOptions.forceWatch is yes
+        aOptions.push "--quiet" if oOptions.quiet is yes
+
+        aOptions.push "--", @data.script
+        aOptions.push oOptions.args... if grunt.util.kindOf( oOptions.args ) is "array"
 
         supervisor.run aOptions
--- tasks/grunt-supervisor.js
+++ tasks/grunt-supervisor.js
@@ -13,11 +13,47 @@ supervisor = require("supervisor");
 
 module.exports = function(grunt) {
   return grunt.registerMultiTask("supervisor", "Runs a supervisor monitor of your node.js server.", function() {
-    var aOptions;
+    var aOptions, oOptions;
     this.async();
     aOptions = [];
-    aOptions.push(this.data.script);
-    console.log(aOptions);
+    oOptions = this.data.options;
+    if (grunt.util.kindOf(oOptions.watch) === "array") {
+      aOptions.push("--watch", oOptions.watch.join(","));
+    }
+    if (grunt.util.kindOf(oOptions.ignore) === "array") {
+      aOptions.push("--ignore", oOptions.ignore.join(","));
+    }
+    if (grunt.util.kindOf(oOptions.pollInterval) === "number") {
+      aOptions.push("--poll-interval", "" + oOptions.pollInterval);
+    }
+    if (grunt.util.kindOf(oOptions.extensions) === "array") {
+      aOptions.push("--extensions", oOptions.extensions.join(","));
+    }
+    if (grunt.util.kindOf(oOptions.exec) === "string") {
+      aOptions.push("--exec", oOptions.exec);
+    }
+    if (oOptions.debug === true) {
+      aOptions.push("--debug");
+    }
+    if (oOptions.debugBrk === true) {
+      aOptions.push("--debug-brk");
+    }
+    if (oOptions.harmony === true) {
+      aOptions.push("--harmony");
+    }
+    if (grunt.util.kindOf(oOptions.noRestartOn) === "string" && (oOptions.noRestartOn === "error" && oOptions.noRestartOn === "exit")) {
+      aOptions.push("--no-restart-on", oOptions.noRestartOn);
+    }
+    if (oOptions.forceWatch === true) {
+      aOptions.push("--force-watch");
+    }
+    if (oOptions.quiet === true) {
+      aOptions.push("--quiet");
+    }
+    aOptions.push("--", this.data.script);
+    if (grunt.util.kindOf(oOptions.args) === "array") {
+      aOptions.push.apply(aOptions, oOptions.args);
+    }
     return supervisor.run(aOptions);
   });
 };
